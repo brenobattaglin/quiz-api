23b2af2bafed4243ff3288302ab3c4a57a787c66
Breno Battaglin <breno.battaglin@icloud.com>
2021-08-10 22:16:01 -0300

    Create server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('database connected'))
+
+app.listen(process.env.PORT, () => {
+    console.log("The API is running...")
+})
