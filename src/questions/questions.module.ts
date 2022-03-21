d2c7b28eff1a29a3ec76b9c709ccbe365353e295
Breno Battaglin <breno.battaglin@icloud.com>
2022-03-21 20:41:29 -0300

    Update question module

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { questionProviders } from './questions.provider';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [QuestionsController],
-  providers: [QuestionsService]
+  providers: [QuestionsService, ...questionProviders],
 })
 export class QuestionsModule {}
