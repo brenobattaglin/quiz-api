b4091a74040681fe270e030eb91ddf126496e55b
Breno Battaglin <breno.battaglin@icloud.com>
2022-04-26 21:10:29 -0300

    Delete app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [QuestionsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
