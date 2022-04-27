15d7b2959a7aca609ebb6a7a33372d9941202770
Breno Battaglin <breno.battaglin@icloud.com>
2022-04-26 21:44:51 -0300

    Delete database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionsModule } from './questions/questions.module';
-import { DatabaseModule } from './database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://localhost/nest'),
     QuestionsModule,
-    DatabaseModule,
   ],
 })
 export class AppModule {}
--- src/database/database.providers.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const databaseProvider = [
-  {
-    provide: databaseConnection,
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb+srv://localhost/nest'),
-  },
-];
